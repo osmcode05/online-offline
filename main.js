ebe188b0722a23d0992273fced19d5384004c2d8
oussama <ousamatessla@gmail.com>
2024-06-01 00:10:25 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+let h1 = document.querySelector('h1'),
+    ul = document.querySelector('ul'),
+    button = document.querySelector('button');
+onload=()=>{(navigator.onLine)?online():offline()}
+ononline = ()=> online()
+onoffline = ()=> offline()
+function online(){                                       
+    h1.innerHTML = 'Online Now';
+    h1.style.color = 'green';
+    ul.style.display = 'none';
+    button.style.display = 'none';
+}
+function offline(){
+    h1.innerHTML = 'Offline Now';
+    h1.style.color = 'black';
+    ul.style.display = 'block';
+    button.style.display = 'block';
+}
+
+
